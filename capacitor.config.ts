19adbacccbf07e0ed6e7b013768b0bd1e1489a26
RamonArrazola <jarrazola@exsoinf.com>
2024-09-30 11:21:50 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pushApp',
+  webDir: 'www'
+};
+
+export default config;
